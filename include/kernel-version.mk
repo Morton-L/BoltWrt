4d32e360c15b1d30263d186e5b31b3b603135214
沫延 <Morton.L@Outlook.com>
2021-01-30 13:32:16 +0800

    imagebuilder: pass IB=1 on checking requirements 内核: 由 4.14 升级为 4.14.217 wireguard: Fix compile with kernel 4.14.217 odhcp6c: fix IPv6 routing loop on point-to-point links netifd: fix IPv6 routing loop on point-to-point links dnsmasq: backport fixes

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,9 +6,9 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-4.14 = .215
+LINUX_VERSION-4.14 = .217
 
-LINUX_KERNEL_HASH-4.14.215 = 9a844e290c61b39dd23df507ba1d9c2b2b4365e5993a978b19f1eb5610b926da
+LINUX_KERNEL_HASH-4.14.217 = 62a36a25431016d98d0f1cff98be432086c51f86fd79042bd10a867b3a924d11
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
